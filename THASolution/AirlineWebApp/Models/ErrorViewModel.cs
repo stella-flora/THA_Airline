c01f984e29cf9b8e064d4f0a91c5ba6cb67eeffb
Stella Flora <Stella.Flora.c10418@mcast.edu.mt>
2023-11-18 11:34:40 +0100

    1 - Set up git, Create solution and webapp

---
+++ THASolution/AirlineWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirlineWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
